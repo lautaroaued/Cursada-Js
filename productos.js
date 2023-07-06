0705fd8fab83776d0c466887b5d1b8830b9e6c42
Lautaro Aued <lautaromaued@gmail.com>
2023-07-05 23:18:26 -0300

    tercera entrega

--- productos.js
+++ productos.js
@@ -1,5 +1,4 @@
-
-let item = localStorage.getItem('Favoritos');
+let item = localStorage.getItem("Favoritos");
 let elemento = JSON.parse(item);
 console.log(elemento);
 
@@ -42,7 +41,6 @@ for (let i = 0; i < elemento.length; i++) {
 }
 
 document.getElementById("productos-seleccionados").appendChild(tabla);
-
 
 function eliminarElemento(index) {
  
@@ -87,32 +85,58 @@ function eliminarElemento(index) {
     tablaActualizada.appendChild(fila);
   }
 
-  let productosSeleccionados = document.getElementById("productos-seleccionados");
-  productosSeleccionados.innerHTML = ""; 
+  let productosSeleccionados = document.getElementById(
+    "productos-seleccionados"
+  );
+  productosSeleccionados.innerHTML = "";
   productosSeleccionados.appendChild(tablaActualizada);
-  mostrarImporteTotal()
+  mostrarImporteTotal();
 }
 
-
 function mostrarImporteTotal() {
-    const totalMonto = elemento.reduce((acc, elem) => acc + elem.precio, 0);
-  
-    let totalMontoElemento = document.createElement("p");
-    totalMontoElemento.textContent = `Monto Total a pagar por los seguros contratados es: $${totalMonto}`;
-    
-    let montoTotalContainer = document.getElementById("monto-total").firstElementChild;
-    montoTotalContainer.textContent = "";
-    montoTotalContainer.appendChild(totalMontoElemento);
-  }
+  const totalMonto = elemento.reduce((acc, elem) => acc + elem.precio, 0);
+
+  let totalMontoElemento = document.createElement("p");
+  totalMontoElemento.textContent = `Monto Total a pagar por los seguros contratados es: $${totalMonto}`;
+
+  let montoTotalContainer =
+    document.getElementById("monto-total").firstElementChild;
+  montoTotalContainer.textContent = "";
+  montoTotalContainer.appendChild(totalMontoElemento);
+}
 
   mostrarImporteTotal();
   
-
-
-
+  const botonFinalizar = document.getElementById('boton-finalizar');
   
-
-
+  botonFinalizar.addEventListener('click', async function() {
+    const { value: email } = await Swal.fire({
+      title: 'Ingresa tu correo electrónico para que te enviemos las pólizas',
+      input: 'email',
+      inputLabel: 'Correo electrónico',
+      inputPlaceholder: 'Ingresa tu dirección de email'
+    });
+  
+    if (email) {
+      Swal.fire(`Se enviaron las pólizas a: ${email}`)
+      
+      .then(() => {
+        Swal.fire({
+          title: '¿Deseas contratar un seguro mas?',
+          showDenyButton: true,
+          showCancelButton: true,
+          confirmButtonText: 'Seguir contratando',
+          denyButtonText: 'No, gracias'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = 'tabla.html';
+          } else if (result.isDenied) {
+            Swal.fire('Gracias por su contratacion, vuelva pronto' );
+          }
+        });
+      });
+    }
+  });
 
 
 
